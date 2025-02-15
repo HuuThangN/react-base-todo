040ab1bf1d92a8d97cc609cfec4670502fb8d4fe
Nguyen Huu Thang <nhthangvp96@gmail.com>
2025-02-15 17:07:13 +0700

    how to use props

---
+++ src/components/todo/TodoNew.jsx
@@ -0,0 +1,10 @@
+const TodoNew = () => {
+    return (
+        <div className="todo-new">
+            <input type="text" name="" id="" />
+            <button>Add</button>
+        </div>
+    );
+};
+
+export default TodoNew;
