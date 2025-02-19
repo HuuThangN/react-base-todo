98215f9ba282b39030cabd610af5c60b28936610
Nguyen Huu Thang <nhthangvp96@gmail.com>
2025-02-19 16:27:36 +0700

    add header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import TodoNew from './components/todo/TodoNew';
 import TodoData from './components/todo/TodoData';
+import Header from './components/layout/header';
 import './components/todo/todo.css';
 import reactLogo from './assets/react.svg';
 import { useState } from 'react';
-
+import Footer from './components/layout/footer';
 const App = () => {
     const [todoList, setTodoList] = useState([
         // { id: 1, name: 'Learning React' },
@@ -26,20 +27,24 @@ const App = () => {
     };
 
     return (
-        <div className="todo-container">
-            <div className="todo-title">Todo List</div>
+        <>
+            <Header />
+            <div className="todo-container">
+                <div className="todo-title">Todo List</div>
 
-            <TodoNew addNewTodo={addNewTodo} />
-            {todoList.length > 0 ? (
-                <TodoData todoList={todoList} removeElement={removeElement} />
-            ) : (
-                todoList.length === 0 && (
-                    <div className="todo-image">
-                        <img className="logo" src={reactLogo} alt="" />
-                    </div>
-                )
-            )}
-        </div>
+                <TodoNew addNewTodo={addNewTodo} />
+                {todoList.length > 0 ? (
+                    <TodoData todoList={todoList} removeElement={removeElement} />
+                ) : (
+                    todoList.length === 0 && (
+                        <div className="todo-image">
+                            <img className="logo" src={reactLogo} alt="" />
+                        </div>
+                    )
+                )}
+            </div>
+            <Footer />
+        </>
     );
 };
 
